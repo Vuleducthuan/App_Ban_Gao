6df200a563a1c59179d4258869e8a75073d6e0f7
Lawrence-Miyato <133018250+Lawrence-Miyato@users.noreply.github.com>
2024-12-16 20:51:52 +0700

    HomeScreenHomeScreen

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -7,8 +7,58 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('fsdfsfs'),
+        title: const Text(
+          'App Bán Gạo',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+          ),
+        ),
+        backgroundColor: Colors.pink[50],
       ),
+      body: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                'Đề Xuất',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Text(
+                'Carousel',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Bán Chạy',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Carousel',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
