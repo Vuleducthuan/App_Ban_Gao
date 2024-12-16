700522e0cf9f7eba60ddc031091dcae772e9c1e0
Lawrence-Miyato <133018250+Lawrence-Miyato@users.noreply.github.com>
2024-12-16 20:30:16 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:app_ban_gao/screen/Homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Ban Gao',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Homescreen(title: 'App Ban Gao'),
+    );
+  }
+}
